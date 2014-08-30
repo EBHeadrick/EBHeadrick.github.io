6e3bcce695dbfcbec7447991bd56de36050db109
Ellen B Headrick <EBHeadrick@hotmail.com>
2014-08-30 15:05:07 -0600

    Making Portfolio with a link to BIG so I can show several different things

---
+++ BIG/js/BIG.js
@@ -0,0 +1,12 @@
+/**
+ * Created by EB on 8/6/2014.
+ */
+
+
+$(function() {
+
+    $('#bigLogo').on('click', function(){
+        $('#homePill').addClass('active');
+    });
+
+}) // end of function for the document ready
